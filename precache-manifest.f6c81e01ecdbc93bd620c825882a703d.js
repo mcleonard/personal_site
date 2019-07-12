a7d503bf75cc647eeb849154bc689114076cd80f
Mathew Leonard <leonard.mat@gmail.com>
2019-07-12 07:39:27 -0700

    Updates

---
+++ precache-manifest.f6c81e01ecdbc93bd620c825882a703d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898b702bca70930e576dd1eada0f2085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5529c4c40a9d0c574c4e",
+    "url": "/static/css/main.72a088a9.chunk.css"
+  },
+  {
+    "revision": "22cfa940b2bb7e757f75",
+    "url": "/static/js/2.62212d98.chunk.js"
+  },
+  {
+    "revision": "5529c4c40a9d0c574c4e",
+    "url": "/static/js/main.0085919c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50bbc7ac5b60b294fb8909e7b2a1033a",
+    "url": "/static/media/Probability-Matrix-Factorization.50bbc7ac.ipynb"
+  },
+  {
+    "revision": "c172841e3e7a3aa1397d041d484c22b5",
+    "url": "/static/media/Probability-Matrix-Factorization.c172841e.meta"
+  },
+  {
+    "revision": "86adfe429059b11d6468c6c65db60ffa",
+    "url": "/static/media/PyTorch-MCMC.86adfe42.meta"
+  },
+  {
+    "revision": "927bb2cd1a1297f76026bdf366bc26c2",
+    "url": "/static/media/PyTorch-MCMC.927bb2cd.ipynb"
+  },
+  {
+    "revision": "d9836dc4a45ed526c4cef9ebd00c481a",
+    "url": "/static/media/Rendering Markdown and LaTeX.d9836dc4.meta"
+  },
+  {
+    "revision": "ff204b5693dec97dbf23915ffbae4efe",
+    "url": "/static/media/Rendering Markdown and LaTeX.ff204b56.ipynb"
+  },
+  {
+    "revision": "7ff9ff3219fe047f28a2b6eced5c3ac1",
+    "url": "/static/media/ab_testing_with_sampyl.7ff9ff32.meta"
+  },
+  {
+    "revision": "aa0065ae85ae5cc25d8c3495837fc8bc",
+    "url": "/static/media/ab_testing_with_sampyl.aa0065ae.ipynb"
+  },
+  {
+    "revision": "75f14d96c7ba06486e3c924de0e5bb02",
+    "url": "/static/media/body_fat_percentage.75f14d96.ipynb"
+  },
+  {
+    "revision": "cc8228935a1cacdfc82342998df879e9",
+    "url": "/static/media/build_meta.cc822893.py"
+  },
+  {
+    "revision": "3b060fcb91decb378537c506d22ea3cc",
+    "url": "/static/media/chris-cohen.3b060fcb.png"
+  },
+  {
+    "revision": "e1980d821bbd77b8b882f843b1e06e66",
+    "url": "/static/media/github_hosting_documentation.e1980d82.ipynb"
+  },
+  {
+    "revision": "06f7db21de792833b5c07554b2f21526",
+    "url": "/static/media/janelle-passmore.06f7db21.png"
+  },
+  {
+    "revision": "1102fac359e4607aa8f215fc41db4e7f",
+    "url": "/static/media/mat_profile_img.1102fac3.png"
+  },
+  {
+    "revision": "c1c857c6d3a1b87855bb5174d5d50577",
+    "url": "/static/media/p_values_statistical_testing.c1c857c6.ipynb"
+  },
+  {
+    "revision": "e13885ebd86775e98a42fa1328d086ac",
+    "url": "/static/media/reward.e13885eb.png"
+  },
+  {
+    "revision": "06636d11a9b263564af1c76a8af091e1",
+    "url": "/static/media/sampyl-example.06636d11.png"
+  },
+  {
+    "revision": "39d098e6efd4743acfa304164a547649",
+    "url": "/static/media/ventura.39d098e6.png"
+  },
+  {
+    "revision": "b2a3a09af576269c6f6d85c252efd20a",
+    "url": "/static/media/yelp_timeline.b2a3a09a.ipynb"
+  }
+]);
