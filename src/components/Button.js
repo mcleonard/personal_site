79c44db3524eed501b220b9fea7a39f6f8f447ed
Mathew Leonard <leonard.mat@gmail.com>
2019-07-12 09:00:59 -0700

    Add functionality for internal and external links

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Button.css'
 
 function Button(props) {
 
-    return (
+    let link = (props.route ? 
+        <Link to={props.link} className='Button'
+              target={(props.new_tab ? "_blank" : "")}
+              rel={(props.new_tab ? "noopener noreferrer": "")}>
+        {props.text}
+        </Link>
+        : 
         <a href={props.link} className='Button'
-            target={(props.new_tab ? "_blank" : "")}
-            rel={(props.new_tab ? "noopener noreferrer": "")}>
-            {props.text}
+           target={(props.new_tab ? "_blank" : "")}
+           rel={(props.new_tab ? "noopener noreferrer": "")}>
+        {props.text}
         </a>
-    )
+        )
+
+    return link
 }
 
-Button.defaultProps = {new_tab: true}
+Button.defaultProps = {new_tab: true,
+                       route: false}
 
 export default Button
