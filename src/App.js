7ac3aa493dd5c2fe6a2f2e61838d05fdad3fdc74
Mathew Leonard <leonard.mat@gmail.com>
2019-07-12 08:07:32 -0700

    Use hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { HashRouter as Router, Route} from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
 import Home from './content/Home';
